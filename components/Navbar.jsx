d2918b3a427b06399f15780cabaa2803ed36d894
Priyanshu-Kapadia <priyanshu.kapadia@gmail.com>
2023-01-05 15:52:36 +0530

    Error Solve

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,14 +9,14 @@ import { navVariants } from '../utils/motion';
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
-    initial='hidden'
-    whileInView='show'
+    initial="hidden"
+    whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
-        src='/search.svg'
+        src="/search.svg"
         alt="search"
         className="w-[24] h-[24px] object-contain"
       />
@@ -24,7 +24,7 @@ const Navbar = () => (
         METAVERUS
       </h2>
       <img
-        src='/menu.svg'
+        src="/menu.svg"
         alt="menu"
         className="w-[24] h-[24px] object-contain"
       />
